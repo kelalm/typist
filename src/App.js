1fcc3fc8c4afb20841ff6a7b876b905adcadb05c
kelalm <kelvinalmonte@live.com>
2020-02-21 23:05:06 -0500

    changed site title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="heading">Getting Started</h1>
         <p>
           Welcome to <code>Typist.</code> Start as a guest, or create an
           account.
