6b5acd1777d565d6f668616a5c1dc0dddf123d29
kelalm <kelvinalmonte@live.com>
2020-02-24 20:36:30 -0500

    alternating login and create account text on page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
+import { Button } from "shards-react";
 
 import LoginForm from "../LoginForm";
 import SignUpForm from "../SignUpForm";
 
 function Login() {
   const [mode, setMode] = useState("Login");
+  const [modeButton, setModeButton] = useState("Create an account");
+
+  function handleModeClick(event) {
+    mode == "Login" ? setMode("Create an account") : setMode("Login");
+    modeButton == "Login"
+      ? setModeButton("Create an account")
+      : setModeButton("Login");
+  }
 
   return (
     <div className="App">
@@ -12,6 +21,9 @@ function Login() {
         <h1 className="heading">{mode}</h1>
         <LoginForm></LoginForm>
         <SignUpForm></SignUpForm>
+        <Button theme="dark" onClick={handleModeClick}>
+          {modeButton}
+        </Button>
       </header>
     </div>
   );
