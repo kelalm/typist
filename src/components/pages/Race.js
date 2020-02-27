bf3781aef4ce376be82cf4f53b15c88b704cebff
kelalm <kelvinalmonte@live.com>
2020-02-26 20:48:16 -0500

    added race page

---
+++ src/components/pages/Race.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+export default function Race() {
+  return (
+    <div>
+      <h1 className="heading">Getting Started</h1>
+    </div>
+  );
+}
