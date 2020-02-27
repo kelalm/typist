177476ed8b5f0f49b5854ecffefdab355b6ae8ad
kelalm <kelvinalmonte@live.com>
2020-02-26 20:56:57 -0500

    added entry box component

--- src/components/pages/Race.js
+++ src/components/pages/Race.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import ParagraphCard from "../ParagraphCard";
+import EntryBox from "../EntryBox";
 
 export default function Race() {
   return (
